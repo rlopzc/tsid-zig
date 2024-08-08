871e356046baf010b5660f65dd396be1292f452b
Romario López <romariolopezc@gmail.com>
2024-08-07 22:40:45 -0700

    Update example

--- examples/tsid_factory.zig
+++ examples/tsid_factory.zig
@@ -5,5 +5,5 @@ pub fn main() !void {
     var tsid_factory = TsidFactory.init_1024_nodes(1);
 
     const id = tsid_factory.create();
-    try std.io.getStdOut().writer().print("tsid: {d}\n", .{id});
+    try std.io.getStdOut().writer().print("TSID\nDecimal: {d}\nHex:     {x}\nBinary:  {b}\n", .{ id, id, id });
 }
