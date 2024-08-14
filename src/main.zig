9a6372fb7b5b662cb5f5fd958ef45b7c29e7861d
Romario López <romariolopezc@gmail.com>
2024-08-13 23:52:56 -0600

    Add TSID.toBytes fn

--- src/main.zig
+++ src/main.zig
@@ -30,7 +30,18 @@ pub const TSID = struct {
     pub fn new(number: u64) TSID {
         return TSID{ .number = number };
     }
+
+    pub fn toBytes(self: TSID) [8]u8 {
+        return std.mem.toBytes(self.number);
+    }
 };
+
+test "TSID toBytes" {
+    var factory = TsidFactory.init_256_nodes(1);
+    const tsid = factory.create();
+
+    try testing.expect(tsid.number == std.mem.bytesToValue(u64, tsid.toBytes()[0..]));
+}
 
 pub const TsidFactory = struct {
     node_id: u32,
