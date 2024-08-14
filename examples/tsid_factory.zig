681e90ee5f7a12080d975b32bd7299f14a6fcd8b
Romario López <romariolopezc@gmail.com>
2024-08-14 08:45:22 -0600

    Fix example

--- examples/tsid_factory.zig
+++ examples/tsid_factory.zig
@@ -4,6 +4,6 @@ const TsidFactory = @import("tsid").TsidFactory;
 pub fn main() !void {
     var tsid_factory = TsidFactory.init_1024_nodes(1);
 
-    const id = tsid_factory.create();
+    const id = tsid_factory.create().number;
     try std.io.getStdOut().writer().print("TSID\nDecimal: {d}\nHex:     {x}\nBinary:  {b}\n", .{ id, id, id });
 }
